8095c7de94521197673c0421e2d4a8db56327eca
Aleksandr Pyzh <pyzh98@gmail.com>
2019-02-15 16:52:43 +0200

    Loading to github

---
+++ XML/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace XML
+{
+    public partial class Form1 : Form
+    {
+        static Yml_catalog catalog;
+
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "XML (*.xml) | *.xml";
+            saveFileDialog1.Filter = "XML (*.xml) | *.xml";
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            string filePatch = openFileDialog1.FileName;
+
+            comboBox1.Items.Clear();
+
+            using (var stream = new StreamReader(filePatch))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Yml_catalog));
+
+                try
+                {
+                    catalog = (Yml_catalog)serializer.Deserialize(stream);
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка!\nНеверная структура файла.");
+                    return;
+                }
+            }    
+            
+            foreach(var i in catalog.Shop.Categories.Category)
+            {
+                comboBox1.Items.Add(i.Text);
+            }
+
+            label2.Text = filePatch;
+            button3.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string selectedItemText = comboBox1.SelectedItem.ToString();
+
+            var selectedItemId = catalog.Shop.Categories.Category.Where(i => i.Text == selectedItemText).FirstOrDefault().Id;
+
+            var itemsToChange = catalog.Shop.Offers.Offer.Where(i => i.CategoryId == selectedItemId);
+
+            foreach (var item in itemsToChange)
+            {
+                double price;
+
+                double percentValue;
+
+                try
+                {
+                    double.TryParse(item.Price.Replace('.', ','), out price);
+
+                    double.TryParse(textBox1.Text, out percentValue);
+
+                    int newPrice = (int)price + (int)(price * (percentValue / 100));
+
+                    item.Price = newPrice.ToString() + ".00";
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Произошла ошибка!");
+                }
+            }
+
+            MessageBox.Show("Наценка применена");
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && number != 44) //цифры, клавиша BackSpace и запятая а ASCII
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            string saveFilePatch = saveFileDialog1.FileName;
+
+            XmlSerializer formatter = new XmlSerializer(typeof(Yml_catalog));
+
+            string xml;
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                formatter.Serialize(textWriter, catalog);
+
+                xml = textWriter.ToString();
+            }
+
+            xml = xml.Replace("<?xml version=\"1.0\" encoding=\"utf-16\"?>", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            xml = xml.Replace(" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"", "");
+            xml = xml.Replace("<sales_notes />", "<sales_notes/>Возможна покупка менее 4 шт по согласованию с менеджером. Гарантия производителя");
+
+            using (StreamWriter sw = new StreamWriter(saveFilePatch, false, System.Text.Encoding.UTF8))
+            {
+                sw.WriteLine(xml);
+            }
+
+            MessageBox.Show("Файл успешно сохранен!\n\n" + saveFilePatch);
+            GC.Collect(2);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+        }
+    }
+}
---
+++ XML/Models.cs
@@ -0,0 +1,137 @@
+﻿using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace XML
+{
+    [XmlRoot(ElementName = "script")]
+    public class Script
+    {
+        [XmlAttribute(AttributeName = "type")]
+        public string Type { get; set; }
+        [XmlAttribute(AttributeName = "charset")]
+        public string Charset { get; set; }
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+    }
+
+    [XmlRoot(ElementName = "currency")]
+    public class Currency
+    {
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+        [XmlAttribute(AttributeName = "rate")]
+        public string Rate { get; set; }
+    }
+
+    [XmlRoot(ElementName = "currencies")]
+    public class Currencies
+    {
+        [XmlElement(ElementName = "currency")]
+        public Currency Currency { get; set; }
+    }
+
+    [XmlRoot(ElementName = "category")]
+    public class Category
+    {
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "categories")]
+    public class Categories
+    {
+        [XmlElement(ElementName = "category")]
+        public List<Category> Category { get; set; }
+    }
+
+    [XmlRoot(ElementName = "param")]
+    public class Param
+    {
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "offer")]
+    public class Offer
+    {
+        [XmlElement(ElementName = "productId")]
+        public string ProductId { get; set; }
+        [XmlElement(ElementName = "url")]
+        public string Url { get; set; }
+        [XmlElement(ElementName = "price")]
+        public string Price { get; set; }
+        [XmlElement(ElementName = "currencyId")]
+        public string CurrencyId { get; set; }
+        [XmlElement(ElementName = "categoryId")]
+        public string CategoryId { get; set; }
+        [XmlElement(ElementName = "picture")]
+        public string Picture { get; set; }
+        [XmlElement(ElementName = "name")]
+        public string Name { get; set; }
+        [XmlElement(ElementName = "vendor")]
+        public string Vendor { get; set; }
+        [XmlElement(ElementName = "brand_id")]
+        public string Brand_id { get; set; }
+        [XmlElement(ElementName = "vendorCode")]
+        public string VendorCode { get; set; }
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+        [XmlElement(ElementName = "cpa")]
+        public string Cpa { get; set; }
+        [XmlElement(ElementName = "param")]
+        public List<Param> Param { get; set; }
+        [XmlElement(ElementName = "model")]
+        public string Model { get; set; }
+        [XmlElement(ElementName = "sales_notes")]
+        public string Sales_notes { get; set; }
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+        [XmlAttribute(AttributeName = "available")]
+        public string Available { get; set; }
+    }
+
+    [XmlRoot(ElementName = "offers")]
+    public class Offers
+    {
+        [XmlElement(ElementName = "offer")]
+        public List<Offer> Offer { get; set; }
+    }
+
+    [XmlRoot(ElementName = "shop")]
+    public class Shop
+    {
+        [XmlElement(ElementName = "name")]
+        public string Name { get; set; }
+        [XmlElement(ElementName = "company")]
+        public string Company { get; set; }
+        [XmlElement(ElementName = "url")]
+        public string Url { get; set; }
+        [XmlElement(ElementName = "platform")]
+        public string Platform { get; set; }
+        [XmlElement(ElementName = "version")]
+        public string Version { get; set; }
+        [XmlElement(ElementName = "email")]
+        public string Email { get; set; }
+        [XmlElement(ElementName = "currencies")]
+        public Currencies Currencies { get; set; }
+        [XmlElement(ElementName = "categories")]
+        public Categories Categories { get; set; }
+        [XmlElement(ElementName = "offers")]
+        public Offers Offers { get; set; }
+    }
+
+    [XmlRoot(ElementName = "yml_catalog")]
+    public class Yml_catalog
+    {
+        [XmlElement(ElementName = "script")]
+        public Script Script { get; set; }
+        [XmlElement(ElementName = "shop")]
+        public Shop Shop { get; set; }
+        [XmlAttribute(AttributeName = "date")]
+        public string Date { get; set; }
+    }
+}
